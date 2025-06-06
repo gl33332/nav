b3c494f9bcda462f16b059146a953e87d4797313
gl33332 <gl33332@gmail.com>
2025-06-06 23:59:45 +0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,18 @@
 module.exports = function(grunt) {
-  // Project configuration
+  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    // Example task: Minify JavaScript files
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
+    // Example task
+    uglify:min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task
+    grunt.loadNpmTasks('grunt {
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'src/app.js',
+       -contrib-ugl dest: 'dist/appify.min.js'
       }
     }
   });
@@ -19,4 +22,3 @@ module.exports = function(grunt) {
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
-};
